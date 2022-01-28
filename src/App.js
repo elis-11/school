3fb756a11b753091195ff4b815b831ed2f4ea984
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-28 17:47:31 +0100

    new FAVICON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Elis-11</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <h1>Elis</h1>
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
