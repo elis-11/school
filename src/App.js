7aac79a4d3b829aa2d9025ab09868cc14441efbd
elis-11 <eliza.arzanukaeva@gmail.com>
2022-02-09 10:17:40 +0100

    Router-6

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.scss";
 
 function Home() {
-  return <h2>Home</h2>;
+  return <h1>Elis</h1>;
 }
 
 function About() {
   return <h2>About</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function Projects() {
+  return <h2>Projects</h2>;
 }
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <h1>Elis</h1>
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
