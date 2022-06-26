7946793b88bfc5065fd86accbc85e1ea008188d9
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-26 19:11:13 +0200

    Active - NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,30 @@ import "../App.scss";
 export const Navbar = () => {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/courses">Courses</NavLink>
-      <NavLink to="teachers">Teachers</NavLink>
-      <NavLink to="/funding">Funding</NavLink>
-      <NavLink to="/career">Career</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/blog">Blog</NavLink>
-      <NavLink className="apply" to="/apply">Apply Now</NavLink>
+      <NavLink to="/" 
+            className={(navData) => (navData.isActive ? "active" : "none")} end
+      >Home</NavLink>
+      <NavLink to="/courses" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >Courses</NavLink>
+      <NavLink to="teachers" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >Teachers</NavLink>
+      <NavLink to="/funding" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >Funding</NavLink>
+      <NavLink to="/career" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >Career</NavLink>
+      <NavLink to="/about" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >About</NavLink>
+      <NavLink to="/blog" 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+      >Blog</NavLink>
+      <NavLink className="apply" to="/apply">Apply 
+            className={(navData) => (navData.isActive ? "active" : "none")}
+        Now</NavLink>
     </nav>
   );
 };
