a8b4a9027acb76de5c550988cfb9c1edb6a5ecef
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-27 11:13:33 +0200

    Modified Home.scss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,30 +4,52 @@ import "../App.scss";
 export const Navbar = () => {
   return (
     <nav>
-      <NavLink to="/" 
-            className={(navData) => (navData.isActive ? "active" : "none")} end
-      >Home</NavLink>
-      <NavLink to="/courses" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >Courses</NavLink>
-      <NavLink to="teachers" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >Teachers</NavLink>
-      <NavLink to="/funding" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >Funding</NavLink>
-      <NavLink to="/career" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >Career</NavLink>
-      <NavLink to="/about" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >About</NavLink>
-      <NavLink to="/blog" 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-      >Blog</NavLink>
-      <NavLink className="apply" to="/apply">Apply 
-            className={(navData) => (navData.isActive ? "active" : "none")}
-        Now</NavLink>
+      <NavLink
+        to="/"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/courses"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        Courses
+      </NavLink>
+      <NavLink
+        to="teachers"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        Teachers
+      </NavLink>
+      <NavLink
+        to="/funding"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        Funding
+      </NavLink>
+      <NavLink
+        to="/career"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        Career
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/blog"
+        className={(navData) => (navData.isActive ? "active" : "none")}
+      >
+        Blog
+      </NavLink>
+      <NavLink className="apply" to="/apply">
+        Apply now
+      </NavLink>
     </nav>
   );
 };
