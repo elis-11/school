bc121da44d09b06670d2b806d5d68bd29f2cdc80
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-15 20:34:50 +0200

    Project - SEARCH

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { useDataContext } from "../../context/DataProvider";
 // import {NavLink} from "react-router-dom"
 import "./Projects.scss";
 
 export const Projects = () => {
+  const [search, setSearch] = useState("");
   const { projects } = useDataContext();
 
-  // const schowSingleProject=(project) => {
-  //   setFilteredProject([project])
-  //   updateUrlWithId(project)
-  // }
+  const filteredProgect = projects.filter(
+    (project) =>
+      project.name.toLowerCase().includes(search.toLowerCase()) ||
+      project.description.toLowerCase().includes(search.toLowerCase()) ||
+      project.tools.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="projects">
@@ -21,56 +25,70 @@ export const Projects = () => {
         employers want to see - a rich portfolio that showcases your skills.
       </div>
       <div className="project">
+        .
+        <div className="total">
+          {projects.length} {projects.length === 1 ? "Project" : "Projects"}
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            id="search"
+            type="text"
+            role="search"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />{" "}
+        </form>
         <div className="cards">
-          <div className="total">
-            {projects.length} {projects.length === 1 ? "Project" : "Projects"}
-          </div>
-          <div className="cards">
-            {/* {projects.map((project) => ( */}
-            {projects.reverse().map((project) => (
-              <div className="card" key={project._id}>
-                <div className="image">
-                  <img
-                    src={`/images/projects/${project.image}`}
-                    onMouseEnter={(e) =>
-                      // (e.currentTarget.src = project.secondImage)
-                      (e.currentTarget.src = `/images/projects/${project.secondImage}`)
-                    }
-                    // onMouseOut={(e) => (e.currentTarget.src = project.image)}
-                    onMouseOut={(e) =>
-                      (e.currentTarget.src = `/images/projects/${project.image}`)
-                    }
-                    className="img"
-                    // onClick={() =>schowSingleProject(project)}
-                    alt={project.name}
-                  />
-                </div>
+          {/* {projects.map((project) => ( */}
+          {/* {projects.reverse().map((project) => ( */}
+          {filteredProgect.reverse().map((project) => (
+            <div className="card" key={project._id}>
+              <div className="image">
+                <img
+                  src={`/images/projects/${project.image}`}
+                  onMouseEnter={(e) =>
+                    // (e.currentTarget.src = project.secondImage)
+                    (e.currentTarget.src = `/images/projects/${project.secondImage}`)
+                  }
+                  // onMouseOut={(e) => (e.currentTarget.src = project.image)}
+                  onMouseOut={(e) =>
+                    (e.currentTarget.src = `/images/projects/${project.image}`)
+                  }
+                  className="img"
+                  // onClick={() =>schowSingleProject(project)}
+                  alt={project.name}
+                />
+              </div>
 
-                <div className="content">
-                  <div className="title">{project.name}</div>
-                  <div className="text">
-                    <div>Description: {project.description}</div>
-                    <div>Tools: {project.tools}</div>
-                    <div>
-                      <a href={project.link} target="_blank" rel="noreferrer">
-                        {project.link}
-                      </a>
-                    </div>
-                    <div>
-                      <a href={project.github} target="_blank" rel="noreferrer">
-                        {project.github}
-                      </a>
-                    </div>
-                    <div>
-                      <a href={project.backend} target="_blank" rel="noreferrer">
-{project.backend}
-                      </a>
-                    </div>
+              <div className="content">
+                <div className="title">{project.name}</div>
+                <div className="text">
+                  <div>Description: {project.description}</div>
+                  <div>Tools: {project.tools}</div>
+                  <div>
+                    <a href={project.link} target="_blank" rel="noreferrer">
+                      {project.link}
+                    </a>
+                  </div>
+                  <div>
+                    <a href={project.github} target="_blank" rel="noreferrer">
+                      {project.github}
+                    </a>
+                  </div>
+                  <div>
+                    <a href={project.backend} target="_blank" rel="noreferrer">
+                      {project.backend}
+                    </a>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
