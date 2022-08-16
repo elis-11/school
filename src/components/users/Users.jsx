dde2243d683f32d27e321de003e6b5a1ab8c1f26
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-16 22:31:48 +0200

    Search Users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,33 +1,41 @@
 import { useDataContext } from "../../context/DataProvider";
 import { AiTwotoneEdit, AiFillDelete } from "react-icons/ai";
 import "./Users.scss";
-
+import { useState } from "react";
 
 export const Users = () => {
-  const { users, filter, setFilter } = useDataContext();
+  const { users } = useDataContext();
+  const [search, setSearch] = useState("");
 
-  const onFilterUser = (e) => {
-    setFilter({ ...filter, [e.target.name]: e.target.value });
-  };
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="users">
-      <form className="filter">
-        <div className="name">
-          <input
-            type="text"
-            name="name"
-            placeholder="Search"
-            onChange={onFilterUser}
-          />
-        </div>
+      <form
+        className="search"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          id="search"
+          type="text"
+          role="search"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />{" "}
       </form>
 
       <table>
         <thead>
           <tr>
             <th>
-              Total: {users.length} { users.length === 1 ? "User" : "Users"}
+              Total: {users.length} {users.length === 1 ? "User" : "Users"}
             </th>
             <th>name</th>
             <th>e-mail</th>
@@ -35,7 +43,7 @@ export const Users = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user, _id) => (
+          {filteredUsers.map((user, _id) => (
             <tr key={user._id}>
               <td>
                 <img src={`/images/users/${user.image}`} alt={user.name} />
