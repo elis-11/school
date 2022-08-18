ee4f9f5b01a96d2bdb07a2eec8dd6c00e1527594
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-18 10:54:46 +0200

    NotFound

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import {NavLink} from "react-router-dom";
+import "../App.scss";
+
+export const NotFound = () => {
+  return (
+    <div className="NotFound">
+        <div className="content">
+      {/* <img alt="card" 
+      src={`https://source.unsplash.com/1000x1000/`} /> */}
+      <h2>Page not found</h2>
+      <div className="light"></div>
+      <p>Hmmm, the page you were looking for doesn't seem to exist anymore.</p>
+      <NavLink to="/">Back to Home</NavLink>
+      </div>
+    </div>
+  );
+};
